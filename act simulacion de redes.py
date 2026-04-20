389e835027008ac4618606c4fea766c219450eae
Bautista Papadakis <bautistapapadakis@gmail.com>
2026-04-20 16:17:52 -0300

    corrijo ejercicios sistemas

--- act simulacion de redes.py
+++ act simulacion de redes.py
@@ -1,7 +1,7 @@
 class Nodo:
-    def __init__(self, nombre):
-        self.nombre = nombre
-        self.conexiones = []  
+    def __init__(self, nombre):# define una funcion,init(cuando creás un objeto de la clase),self(objeto que estás creando),nombre(dato que le pasás al objeto)
+        self.nombre = nombre #Guarda el valor del parámetro
+        self.conexiones = []  #guarda una lista de conexiones
 
     def agregar_conexion(self, nodo):
         self.conexiones.append(nodo)
@@ -11,15 +11,15 @@ class Nodo:
         for nodo in self.conexiones:
             nodo.recibir_mensaje(mensaje, self.nombre)
 
-    def recibir_mensaje(self, mensaje, remitente):
-        print(f"{self.nombre} recibió de {remitente}: {mensaje}")
+    def recibir_mensaje(self, mensaje, emisor):
+        print(f"{self.nombre} recibió de {emisor}: {mensaje}")
 
 
 
-servidor = Nodo("Servidor")
-cliente1 = Nodo("Cliente 1")
-cliente2 = Nodo("Cliente 2")
-cliente3 = Nodo("Cliente 3")
+servidor = Nodo("servidor")
+cliente1 = Nodo("cliente 1")
+cliente2 = Nodo("cliente 2")
+cliente3 = Nodo("cliente 3")
 
 
 servidor.agregar_conexion(cliente1)
